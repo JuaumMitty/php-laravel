f8e631fba99b586393399fc2ef9f3e2f3b9ad3c5
JuaumMitty <jjoaovictorpg@gmail.com>
2022-01-20 14:09:28 -0300

    Add named routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'Login';});
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+//app
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produto', function(){ return 'Produtos';})->name('app.produtos');
+});
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
